e839b6ace28f3e3adac5178f39fb411f58062e70
Viktoria <doduchenko29@gmail.com>
2021-06-08 08:34:50 +0300

    add

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 export function getTitle() {
-  const elemText = document.querySelector('.title');
+  let elemText = document.querySelector('.title');
   return elemText.textContent;
 }
 getTitle();
 
 export function getDescription() {
-  const elemAbout = document.querySelector('.about');
+  let elemAbout = document.querySelector('.about');
   return elemAbout.innerText;
 }
 getDescription();
 
 export function getPlans() {
-  const elemPlans = document.querySelector('.plans');
+  let elemPlans = document.querySelector('.plans');
   return elemPlans.innerHTML;
 }
 getPlans();
 
 export function getGoal() {
-  const elemGoal = document.querySelector('.goal');
+  let elemGoal = document.querySelector('.goal');
   return elemGoal.outerHTML;
 }
 getGoal();
